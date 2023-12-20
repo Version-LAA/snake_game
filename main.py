ad17073cb2ba25addfa87439fc5ab992c0799946
Latoya A <lat.jamison@gmail.com>
2023-12-20 09:25:22 -0500

    initial config commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+# initial screen configs
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Version_LAA - Snake Game")
+
+
+
+
+screen.exitonclick()
