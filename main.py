0036560e9ef068106b3dc6d5eb985ccea4cae383
Latoya A <lat.jamison@gmail.com>
2023-12-20 09:42:17 -0500

    initial snake geneartion

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Version_LAA - Snake Game")
 
+#initial snake generation
+all_snakes = []
+current_x = 0
+current_y = 0
+for _ in range(3):
+    new_snake = Turtle(shape="square")
+    new_snake.color("white")
+    new_snake.goto(current_x,current_y)
+    all_snakes.append(new_snake)
+    current_x -= 20
 
 
 
